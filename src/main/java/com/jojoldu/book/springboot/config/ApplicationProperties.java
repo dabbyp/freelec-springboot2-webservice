407c6f843319b0cf269251155a420b253ba5a286
dabbyp <60965242+dabbyp@users.noreply.github.com>
2020-03-01 21:55:16 +0900

    to aws

--- src/main/java/com/jojoldu/book/springboot/config/ApplicationProperties.java
+++ src/main/java/com/jojoldu/book/springboot/config/ApplicationProperties.java
@@ -9,18 +9,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationProperties implements ApplicationRunner {
-    @Value("${key}")
-    private String key;
-
-    public String getKey(){
-        return key;
-    }
-
-    public static String strKey;
+//    @Value("${key}")
+//    private String key;
+//
+//    public String getKey(){
+//        return key;
+//    }
+//
+//    public static String strKey;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("================= key : " + key);
-        strKey = key;
+//        System.out.println("================= key : " + key);
+//        strKey = key;
     }
 }
